351ef81d0cdef42aece967068b86fb6de423e92d
manikantathati111 <manikantathati11@gmail.com>
2026-05-01 13:52:40 +0530

    Add files via upload

---
+++ third angle triangle functions.py
@@ -0,0 +1,5 @@
+def my_function(n1,n2):
+    return 180-(n1+n2)
+n1=int(input("enter first angle:"))
+n2=int(input("enter second angle:"))
+print("third angle =",my_function(n1,n2))
---
+++ third angle triangle.py
@@ -0,0 +1,4 @@
+n1=int(input("enter first angle:"))
+n2=int(input("enter second angle:"))
+n3=180-(n1+n2)
+print("third angle =",n3)
